17da19cccdd60b3615aea963a6c1a8dc04dbbc07
Max Zimmermann <max.zimcody212@gmx.de>
2019-05-24 13:20:33 +0200

    sonderregalanpassung

--- raeume/schlafzimmer/sondergealansicht.php
+++ raeume/schlafzimmer/sondergealansicht.php
@@ -61,37 +61,61 @@ include '../../htmlheader.php';?>
     <div id="regalansicht">
 
         <form action="fachansicht.php" method="post">
-            <table id="tabelle">
-                <tr>
-                    <td><button id="sf00" name="fach[]" value="<?php echo $regalnummer; ?>_00" class='fach'></button></td>
-                    <td><button id="sf01" name="fach[]" value="<?php echo $regalnummer; ?>_01" class='fach'></button></td>
-                    <td><button id="sf02" name="fach[]" value="<?php echo $regalnummer; ?>_02" class='fach'></button></td>
-                </tr>
-                <tr>
-                    <td><button id="sf11" name="fach[]" value="<?php echo $regalnummer; ?>_03" class='fach'></button></td>
-                </tr>
-                <tr>
-                    <td><button id="sf20" name="fach[]" value="<?php echo $regalnummer; ?>_04" class='fach'></button></td>
-                    <td><button id="sf21" name="fach[]" value="<?php echo $regalnummer; ?>_05" class='fach'></button></td>
-                    <td><button id="sf22" name="fach[]" value="<?php echo $regalnummer; ?>_06" class='fach'></button></td>
-                </tr>
-                <tr>
-                    <td><button id="sf31" name="fach[]" value="<?php echo $regalnummer; ?>_07" class='fach'></button></td>
-                </tr>
-                <tr>
-                    <td><button id="sf40" name="fach[]" value="<?php echo $regalnummer; ?>_08" <?php if($_SESSION['regale'][$regalnummer][2][0]==0)echo"disabled class='fachdisabled'";else echo"class='fach'"; ?>></button></td>
-                    <td><button id="sf41" name="fach[]" value="<?php echo $regalnummer; ?>_09" <?php if($_SESSION['regale'][$regalnummer][2][1]==0)echo"disabled class='fachdisabled'";else echo"class='fach'"; ?>></button></td>
-                    <td><button id="sf42" name="fach[]" value="<?php echo $regalnummer; ?>_10" <?php if($_SESSION['regale'][$regalnummer][2][2]==0)echo"disabled class='fachdisabled'";else echo"class='fach'"; ?>></button></td>
-                    <td><button id="sf43" name="fach[]" value="<?php echo $regalnummer; ?>_11" <?php if($_SESSION['regale'][$regalnummer][2][3]==0)echo"disabled class='fachdisabled'";else echo"class='fach'"; ?>></button></td>
-                </tr>
-                <tr>
-                    <td><button id="sf50" name="fach[]" value="<?php echo $regalnummer; ?>_12" <?php if($_SESSION['regale'][$regalnummer][3][0]==0)echo"disabled class='fachdisabled'";else echo"class='fach'"; ?>></button></td>
-                    <td><button id="sf51" name="fach[]" value="<?php echo $regalnummer; ?>_13" <?php if($_SESSION['regale'][$regalnummer][3][1]==0)echo"disabled class='fachdisabled'";else echo"class='fach'"; ?>></button></td>
-                    <td><button id="sf52" name="fach[]" value="<?php echo $regalnummer; ?>_14" <?php if($_SESSION['regale'][$regalnummer][3][2]==0)echo"disabled class='fachdisabled'";else echo"class='fach'"; ?>></button></td>
-                    <td><button id="sf53" name="fach[]" value="<?php echo $regalnummer; ?>_15" <?php if($_SESSION['regale'][$regalnummer][3][3]==0)echo"disabled class='fachdisabled'";else echo"class='fach'"; ?>></button></td>
-                </tr>
+            <div id="sondertabelle">
+                <div id="reihe_0" class="reihe">
+                    <div id="sf00" class="sonderdivbutton">
+                        <button id="sb00" name="fach[]" value="<?php echo $regalnummer; ?>_00" class='fach'></button>
+                        <button id="sb01" name="fach[]" value="<?php echo $regalnummer; ?>_01" class='fach'></button>
+                    </div>
+                    <div  id="sf01" class="sonderdivbutton">
+                        <button id="sb02" name="fach[]" value="<?php echo $regalnummer; ?>_02" class='fach'></button>
+                    </div>
+                    <div id="sf02" class="sonderdivbutton">
+                        <button id="sb03" name="fach[]" value="<?php echo $regalnummer; ?>_03" class='fach'></button>
+                    </div>
+                </div>
+                <div id="reihe_1" class="reihe">
+                    <div id="sf10" class="sonderdivbutton">
+                        <button id="sb10" name="fach[]" value="<?php echo $regalnummer; ?>_04" class='fach'></button>
+                        <button id="sb11" name="fach[]" value="<?php echo $regalnummer; ?>_05" class='fach'></button>
+                    </div>
+                    <div id="sf11" class="sonderdivbutton">
+                        <button id="sb12" name="fach[]" value="<?php echo $regalnummer; ?>_06" class='fach'></button>
+                    </div>
+                    <div id="sf12" class="sonderdivbutton">
+                        <button id="sb13" name="fach[]" value="<?php echo $regalnummer; ?>_07" class='fach'></button>
+                    </div>
+                </div>
+                <div id="reihe_2" class="reihe">
+                    <div id="sf20" class="sonderdivbutton">
+                        <button id="sb20" name="fach[]" value="<?php echo $regalnummer; ?>_08" <?php if($_SESSION['regale'][$regalnummer][2][0]==0)echo"disabled class='fachdisabled'";else echo"class='fach'"; ?>></button>
+                    </div>
+                    <div id="sf21" class="sonderdivbutton">
+                        <button id="sb21" name="fach[]" value="<?php echo $regalnummer; ?>_09" <?php if($_SESSION['regale'][$regalnummer][2][1]==0)echo"disabled class='fachdisabled'";else echo"class='fach'"; ?>></button>
+                    </div>
+                    <div id="sf22" class="sonderdivbutton">
+                        <button id="sb22" name="fach[]" value="<?php echo $regalnummer; ?>_10" <?php if($_SESSION['regale'][$regalnummer][2][2]==0)echo"disabled class='fachdisabled'";else echo"class='fach'"; ?>></button>
+                    </div>
+                    <div id="sf23" class="sonderdivbutton">
+                        <button id="sb23" name="fach[]" value="<?php echo $regalnummer; ?>_11" <?php if($_SESSION['regale'][$regalnummer][2][3]==0)echo"disabled class='fachdisabled'";else echo"class='fach'"; ?>></button>
+                    </div>
+                </div>
+                <div id="reihe_3" class="reihe">
+                    <div id="sf30" class="sonderdivbutton">
+                        <button id="sb30" name="fach[]" value="<?php echo $regalnummer; ?>_12" <?php if($_SESSION['regale'][$regalnummer][3][0]==0)echo"disabled class='fachdisabled'";else echo"class='fach'"; ?>></button>
+                    </div>
+                    <div id="sf31" class="sonderdivbutton">
+                        <button id="sb31" name="fach[]" value="<?php echo $regalnummer; ?>_13" <?php if($_SESSION['regale'][$regalnummer][3][1]==0)echo"disabled class='fachdisabled'";else echo"class='fach'"; ?>></button>
+                    </div>
+                    <div id="sf32" class="sonderdivbutton">
+                        <button id="sb32" name="fach[]" value="<?php echo $regalnummer; ?>_14" <?php if($_SESSION['regale'][$regalnummer][3][2]==0)echo"disabled class='fachdisabled'";else echo"class='fach'"; ?>></button>
+                    </div>
+                    <div id="sf33" class="sonderdivbutton">
+                        <button id="sb33" name="fach[]" value="<?php echo $regalnummer; ?>_15" <?php if($_SESSION['regale'][$regalnummer][3][3]==0)echo"disabled class='fachdisabled'";else echo"class='fach'"; ?>></button>
+                    </div>
+                </div>
 
-            </table>
+            </div>
         </form>
 
     </div>
